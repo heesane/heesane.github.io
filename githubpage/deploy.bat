9f486694a1e102d7d3621c174aa07121d9cd4fb1
heesane <tees3359@gmail.com>
2023-11-02 01:59:39 +0900

    deploy

--- githubpage/deploy.bat
+++ githubpage/deploy.bat
@@ -3,4 +3,7 @@ echo start deploy
 npm run build
 
 xcopy /Y /s /i "C:\Users\win102102\Desktop\GithubPage\githubpage\out\*" "C:\Users\win102102\Desktop\GithubPage\" > DeployLogs.txt 2>&1
-pause
+
+git add .
+git commit -m "deploy"
+git push
--- githubpage/next.config.js
+++ githubpage/next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    reactStrictMode: true,
     output: 'export',
+    trailingSlash: '/',
+    env: {},
 }
 
 module.exports = nextConfig
