fc90d3fc18bb1e5a51b3b72ce839e754579ce8ba
heesane <tees3359@gmail.com>
2023-11-02 01:31:27 +0900

    config

--- githubpage/next.config.js
+++ githubpage/next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const debug = process.env.NODE_ENV !== 'production'
+const repository = "heesane.github.io"
+
+const nextConfig = {
+    reactStrictMode : true,
+    assetPrefix: !debug ? `/${repository}/` : '',
+    trailingSlash: true,
+}
 
 module.exports = nextConfig
