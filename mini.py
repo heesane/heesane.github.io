80c7bac75f997d49e6f0ad1ae3fe85156b835cfd
heesane <tees3359@gmail.com>
2024-04-17 07:30:43 +0900

    auto posting through tistory rss

--- mini.py
+++ mini.py
@@ -1,92 +1,74 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-
-from datetime import datetime
-
+import os
 import html
 import xml.etree.ElementTree as ET
-
-from time import sleep
+import pypandoc
+from datetime import datetime
 import requests
-#<!------------------RSS-----------------!>#
+
+# RSS 피드 가져오기
 feed = requests.get('https://heesang0930.tistory.com/rss')
 
-## feed를 html로 변환해서 저장
+# 피드를 파일로 저장
 with open('feed.xml', 'wb') as f:
     f.write(feed.content)
 
-# 입력 형식 정의
+# 날짜 형식 정의
 input_format = "%a, %d %b %Y %H:%M:%S %z"
-
-# 출력 형식 정의
 output_format = "%Y-%m-%d %H:%M:%S %z"
 
-## feed.xml을 파싱
-root = ET.parse('feed.xml')
-
+# feed.xml 파싱
+root = ET.parse('feed.xml').getroot()
 items = root.findall('.//item')
 
-
-
-for item in items[20:22]:
-    title = item.find('title').text
+for i, item in enumerate(items):
+    title = html.unescape(html.unescape(item.find('title').text))
     link = item.find('link').text
     pubDate = item.find('pubDate').text
     tags = item.findall('category')
-    
-    title = html.unescape(html.unescape(title))
-    print(f"Title: {title}")
-    if title.startswith("["):
-        file_title = title[title.index("]")+1:]
-    filename = title.replace(" ", "-").replace("/", "-")
-    # 유효하지 않은 파일 이름 문자 제거
-    filename = ''.join(c for c in filename if c.isalnum() or c in "-_.")
-    print(f"Link: {link}")
-    
+    description = item.find('description')
+    body = html.unescape(html.unescape(description.text if description is not None else ""))
+    body = body.replace("<br>", "\n")
+
+    # 파일 이름 생성
+    filename = ''.join(c for c in title.replace(" ", "-").replace("/", "-") if c.isalnum() or c in "-_.")
+
+    # 날짜 파싱
     parsed_date = datetime.strptime(pubDate, input_format)
     formatted_date_string = parsed_date.strftime(output_format)
-    print(f"Published Date: {formatted_date_string}")
-    
-    print("Tags: ")
-    for tag in tags:
-        print(html.unescape(html.unescape(tag)).text)
-    content = "---\n"
-    content += f"title: {file_title}\n"
-    content += "author: heesane\n"
+
+    # 내용 구성
+    content = f"---\ntitle: \"{title}\"\nauthor: heesang\n"
     if "프로그래머스" in title:
         content += "platform: programmers\n"
-        content += "level: "
-        temp_title = title.split()
-        for i in range(len(temp_title)):
-            if temp_title[i] == "Level":
-                content += f"Level {temp_title[i+1]}\n"
-        
     elif "백준" in title:
         content += "platform: baekjoon\n"
-        content += "level: \n"
-        temp_title = title.split()
-        for i in range(len(temp_title)):
-            if temp_title[i] == "[백준]":
-                content += f"{temp_title[i+1]}\n"
-        
-    content += f"date: {formatted_date_string}\n"
-    content += f"categories: [\n"
-    content += f"\t{html.unescape(html.unescape(tags[0].text))}\n"
-    content += f"]\n"
-    content += "tags: [\n"
-    for tag in tags:
-        content += f"\t{html.unescape(html.unescape(tag.text))},\n"
-    content += "]\n"
-    content += "toc: true\n"
-    content += "comments: true\n"
-    content += "---\n"
-    # 파일 쓰기
-    with open(filename+".md", 'w', encoding='utf-8') as file:
+    else:
+        content += "platform: \n"
+
+    content += f"date: {formatted_date_string}\ncategories: [{', '.join(html.unescape(html.unescape(tag.text)) for tag in tags)}]\n"
+    content += "tags: [" + ', '.join(f"\"{html.unescape(html.unescape(tag.text))}\"" for tag in tags) + "]\ntoc: true\ncomments: true\n---\n"
+
+    # HTML 파일 생성 및 Markdown 변환
+    html_filename = f"{i}.html"
+    with open(html_filename, 'w', encoding='utf-8') as file:
+        file.write(body)
+    output_filename = f"temp_{i}.md"
+    pypandoc.convert_file(html_filename, 'md', outputfile=output_filename)
+
+    # Markdown 파일 내용 추가
+    with open(output_filename, 'r', encoding='utf-8') as file:
+        content += file.read()
+
+    # 최종 Markdown 파일 저장
+    post_filename = f"_posts/{parsed_date.strftime('%Y-%m-%d')}-{filename}.md"
+    with open(post_filename, 'w', encoding='utf-8') as file:
         file.write(content)
 
-    print(f"File {title} created successfully.")
+    # 임시 파일 삭제
+    os.remove(html_filename)
+    os.remove(output_filename)
+
+    print(f"File {post_filename} created successfully.\n")
 
 
 
